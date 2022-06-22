57fe90bf6dc8f24cfa4ac60b6882af5a4e734b16
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-06-22 18:34:08 -0500

    ART-28 User - UsernameValidate - Generar Dao

--- Controllers/usersServices.js
+++ Controllers/usersServices.js
@@ -1,6 +1,15 @@
-const userDAO = require('../models/usersDAO')
-
-const getAllUsers = (req, res) => {
+const bd =require('../configMysql')
+findByUsername : (username, callback) => {
+    let sql = 'SELECT * FROM users WHERE userName=?'
+    bd.query(sql,username, (err, data) => {
+        if (err)throw err
+        if (data.length > 0)
+            callback(data[0])
+        else
+            callback(null)
+    })
+},
+getAllUsers = (req, res) => {
     userDAO.getAllUsers(data => {
         try {
             if (!data) throw new Err("No existen usuarios")
@@ -14,7 +23,8 @@ const getAllUsers = (req, res) => {
         }
     })
 }
-
 module.exports = {
+    findByUsername,
     getAllUsers,
+    
 }
