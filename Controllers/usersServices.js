3c83beb640aa296a73042af8cf25217c0a173c9b
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-06-27 18:52:52 -0500

    ART-37 User - SignUp - Generar Service

---
+++ Controllers/usersServices.js
@@ -0,0 +1,25 @@
+const userDAO = require('../Models/usersDAO')
+const bcrypt = require('bcrypt')
+
+const signUp = (req, res) => {
+    const user = {
+        nombre: req.body.nombre,
+        apellidoPaterno: req.body.apellidoPaterno,
+        userName: req.body.userName,
+        password: bcrypt.hashSync(req.body.password, 20),
+        fechaNacimiento: req.body.fechaNacimiento,
+    }
+    userDAO.insertUser(user, (data) => {
+        if (data && data.affectedRows === 1) {
+            res.send({
+                status: true,
+                message: 'Usuario creado exitosamente'
+            })
+        } else {
+            res.send({
+                status: false,
+                message: 'Ha ocurrido un error al crear el usuario'
+            })
+        }
+    })
+}
