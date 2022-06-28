c1d5a39cf6dbfc9a613f011c26c2a9f379a1f0e2
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-06-27 19:02:43 -0500

    ART-38 User - SignUp - Generar Router

--- Controllers/usersServices.js
+++ Controllers/usersServices.js
@@ -23,3 +23,7 @@ const signUp = (req, res) => {
         }
     })
 }
+
+module.exports = {
+    signUp,
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userServices = require('../Controllers/usersServices')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/signUp',userServices.signUp)
 
 module.exports = router;
