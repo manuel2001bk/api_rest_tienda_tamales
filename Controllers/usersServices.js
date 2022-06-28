35fb005a08b908af88af871557d5efcba631fb7c
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-06-28 18:14:19 -0500

    ART-39 User - SignUp - Testing

--- Controllers/usersServices.js
+++ Controllers/usersServices.js
@@ -2,26 +2,41 @@ const userDAO = require('../Models/usersDAO')
 const bcrypt = require('bcrypt')
 
 const signUp = (req, res) => {
-    const user = {
-        nombre: req.body.nombre,
-        apellidoPaterno: req.body.apellidoPaterno,
-        userName: req.body.userName,
-        password: bcrypt.hashSync(req.body.password, 20),
-        fechaNacimiento: req.body.fechaNacimiento,
-    }
-    userDAO.insertUser(user, (data) => {
-        if (data && data.affectedRows === 1) {
-            res.send({
-                status: true,
-                message: 'Usuario creado exitosamente'
-            })
-        } else {
-            res.send({
-                status: false,
-                message: 'Ha ocurrido un error al crear el usuario'
-            })
+    try {
+        if (!req.body.nombre || !req.body.apellidoPaterno || !req.body.userName || !req.body.password || !req.body.fechaNacimiento) throw new Error("Datos incompletos")
+        if (!req.body.nombre.match(/^[a-zA-Z]{3,}$/)) throw new Error("Nombre invalido")
+        if (!req.body.apellidoPaterno.match(/^[a-zA-Z]{3,}$/)) throw new Error("Apellido invalido")
+        if (!req.body.userName.match(/^[a-zA-Z0-9]{3,}$/)) throw new Error("Usuario invalido")
+        if (!req.body.password.match(/^[a-zA-Z0-9]{3,}$/)) throw new Error("Contraseña invalida")
+        if (!req.body.fechaNacimiento.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) throw new Error("Fecha de nacimiento invalida")
+
+        const user = {
+            nombre: req.body.nombre,
+            apellidoPaterno: req.body.apellidoPaterno,
+            userName: req.body.userName,
+            password: bcrypt.hashSync(req.body.password, 10),
+            fechaNacimiento: req.body.fechaNacimiento,
         }
-    })
+        userDAO.insertUser(user, (data) => {
+            if (data && data.affectedRows === 1) {
+                res.send({
+                    status: true,
+                    message: 'usuario creado exitosamente'
+                })
+            } else {
+                res.send({
+                    status: false,
+                    message: 'Ha ocurrido un error al crear el usuario'
+                })
+            }
+        })
+    } catch (e) {
+        res.send({
+            status: false,
+            message: "No se pudo crear el usuario",
+            errorCode: e.message
+        })
+    }
 }
 
 module.exports = {
--- Models/usersDAO.js
+++ Models/usersDAO.js
@@ -1,9 +1,11 @@
-const db = require('../configMysql')
+const bd = require('../configMysql')
 
 module.exports = {
     insertUser : (user, callback) => {
+        console.log("ingreso a insertUser")
         let sql = 'INSERT INTO users SET ?'
         bd.query(sql,user, (err, data) => {
+            console.log(callback)
             if (err)
                 return callback(null)
             else
